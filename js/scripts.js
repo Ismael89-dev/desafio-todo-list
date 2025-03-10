1ef77a6732b99625bb3a7a865d6d144439effd44
= <=>
2025-03-10 05:04:15 +0000

    Denovo

--- js/scripts.js
+++ js/scripts.js
@@ -2,21 +2,20 @@ const form = document.getElementById('form')
 const tarefas = [];
 form.addEventListener('submit', e =>{
     e.preventDefault();
-    const tarefa = document.getElementById('tarefa').value;
-
-    if(!tarefa){
+    const tarefa = document.getElementById('tarefa');
+    if(!tarefa.value){
         alert('Preencha o campo da tarefa!');
     }else{
         const listaDeTarefas = document.querySelector('.lista-de-tarefas');
         const result = document.createElement('li');
         const text = document.createElement('span');
         const img = document.createElement('img');
-        text.innerText = tarefa;
+        text.innerText = tarefa.value;
         text.setAttribute('id', 'js-tarefa')
         result.appendChild(text);
         listaDeTarefas.appendChild(result)
+        tarefa.value = "";
     }
-    
 });
 
 const listaDeTarefas = document.querySelector('.lista-de-tarefas');
